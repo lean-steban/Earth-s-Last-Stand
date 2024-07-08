a08a84722b59a7076a41e3340aaf05cc2a8c3893
Dream_VMO <javmo1999@hotmail.com>
2024-07-08 14:26:21 -0500

    final casi
    Retoques

--- bullet.gd
+++ bullet.gd
@@ -1,18 +1,25 @@
 extends Area2D
 
-@export var speed = 500
+@export var speed = 300
 @export var direction = Vector2(1, 0)
+
+# Define los límites del área jugable (ejemplo)
+var area_width = 800
+var area_height = 600
 
 func _ready():
 	set_process(true)
 
 func _process(delta):
 	position += direction * speed * delta
-
-	if position.x > 1024 or position.x < 0 or position.y > 768 or position.y < 0:
+	
+	# Ajusta los límites del área jugable
+	if position.x > area_width or position.x < 0 or position.y > area_height or position.y < 0:
 		queue_free()
+	print("Posición del proyectil:", position)  # Añade esto para depurar la posición del proyectil
 
 func _on_area_entered(area):
-	# Manejar lo que sucede cuando el bullet entra en contacto con un área
-	queue_free()
+	queue_free()  # Eliminar la bala cuando golpea un área
 
+func _on_body_entered(body):
+	queue_free()  # Eliminar la bala cuando golpea un cuerpo
